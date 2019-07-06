78fb108951a21025f6efa3ae3f9bdacb2709504a
ogu5959 <ogu5959@gmail.com>
2019-07-06 14:20:57 +0900

    첫 Commit

---
+++ src/main/java/com/lake/kabang/LakeWorld/LakeWorldApplication.java
@@ -0,0 +1,13 @@
+package com.lake.kabang.LakeWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakeWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LakeWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lake/kabang/LakeWorld/LakeWorldApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lake.kabang.LakeWorld;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LakeWorldApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
